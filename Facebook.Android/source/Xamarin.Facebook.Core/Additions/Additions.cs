e74c8093cb0b3e4b742fdcd1ceb2139ff7c48132
Kirill Lyubimov <dev.lyubimov.kirill@gmail.com>
2018-04-01 18:31:25 +0400

    feat(Core): Core is ready

---
+++ Facebook.Android/source/Xamarin.Facebook.Core/Additions/Additions.cs
@@ -0,0 +1,13 @@
+using Android.Runtime;
+
+namespace Xamarin.Facebook
+{
+    public partial class GraphRequestAsyncTask : global::Android.OS.AsyncTask
+    {
+        protected override Java.Lang.Object DoInBackground (params Java.Lang.Object[] @params)
+        {
+            return new Java.Lang.Object (JNIEnv.CallObjectMethod (Handle, JNIEnv.GetMethodID (JNIEnv.GetObjectClass (Handle), "doInBackground", "([Ljava/lang/Object;)Ljava/lang/Object;")), JniHandleOwnership.TransferLocalRef);
+        }
+    }
+}
+
